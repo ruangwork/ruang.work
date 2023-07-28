743991c63f2dc3d08eb166a2057ed23a018ad6b0
inurhuda00 <inurhuda00@gmail.com>
2023-07-28 18:00:31 +0700

    add bahasa

--- astro-i18next.config.ts
+++ astro-i18next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
   defaultLocale: "en",
-  locales: ["en"],
+  locales: ["en", "id"],
   showDefaultLocale: true,
   namespaces: ["home", "about", "snippets", "blog", "common"],
   defaultNamespace: "common",
